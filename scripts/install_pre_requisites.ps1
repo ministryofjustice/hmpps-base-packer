cd2bbb4cbcb4764c6a99edb9aa157b7b5a182f96
Brett Minnie <github@bdb-studios.co.uk>
2018-10-15 12:57:08 +0100

    Attempt to install firefox

--- scripts/install_pre_requisites.ps1
+++ scripts/install_pre_requisites.ps1
@@ -4,9 +4,6 @@ if (!(Test-Path $ChocoInstallPath)) {
     iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-#---- TEMPORARY ---
-Disable-UAC
-
 #--- Python ---
 choco install python2 -y
 #--- JDK 8 ---
@@ -14,5 +11,13 @@ choco install jdk8 -y
 #--- Maven and gradle
 choco install maven gradle -y
 
-#--- Restore Temporary Settings ---
-Enable-UAC
+# Get firefox esr
+$url = "https://ftp.mozilla.org/pub/firefox/releases/52.9.0esr/win64/en-US/Firefox%20Setup%2052.9.0esr.exe"
+$output = "$env:temp\install-firefox.exe"
+
+$wc = New-Object System.Net.WebClient
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+
+#--- install firefox
+$pathvargs = {$output /S /INI=$env:temp\firefox.ini }
+Invoke-Command -ScriptBlock $pathvargs
