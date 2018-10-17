404a7864dd919077ae88c251055f939d91dae3cf
Brett Minnie <github@bdb-studios.co.uk>
2018-10-17 16:36:07 +0100

    Tweaks and line endings

--- scripts/WindowsManageJenkinsUser.ps1
+++ scripts/WindowsManageJenkinsUser.ps1
@@ -1,13 +1,9 @@
 $ErrorActionPreference = "Stop"
 $VerbosePreference="Continue"
 
-# Update the admin user
-Install-Group Jenkins
-
 $jenkinsCreds = New-Credential -UserName Jenkins -Password "$env:JENKINS_PASSWORD"
 Install-User -Credential $jenkinsCreds
 
 Add-GroupMember -Name Administrators -Member Jenkins
-Add-GroupMember -Name Jenkins -Member Jenkins
 
 Install-Directory -Path "c:\JenkinsHome"
--- scripts/WindowsSlaveInstallSoftware.ps1
+++ scripts/WindowsSlaveInstallSoftware.ps1
@@ -18,6 +18,5 @@ $output = "$env:temp\setup.exe"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
 
-$result = "$env:temp\setup.exe" -ms /INI=c:\temp\firefox.ini
-
+& "$env:temp\setup.exe" -ms /INI=c:\temp\firefox.ini
 #java -jar agent.jar -jnlpUrl http://jenkins.engineering-dev.probation.hmpps.dsd.io:8080/computer/windows_slave/slave-agent.jnlp -secret 82c3364892fc7d5c695de30faa0a1792d0795f7e349c18107de0d030cc94fc42 -workDir "c:/jenkins"
