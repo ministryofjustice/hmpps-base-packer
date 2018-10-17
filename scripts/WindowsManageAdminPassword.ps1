3db351c414f9ac3dd317ced9fe53098186fa0654
Brett Minnie <github@bdb-studios.co.uk>
2018-10-17 12:26:22 +0100

    Fix Carbon Deprecation warning

--- scripts/WindowsManageAdminPassword.ps1
+++ scripts/WindowsManageAdminPassword.ps1
@@ -4,4 +4,5 @@ $VerbosePreference="Continue"
 # Get our admin password
 $adminPass = aws ssm get-parameters --names /dev/jenkins/windows/slave/admin/password --region eu-west-2 --with-decrypt | jq -r '.Parameters[0].Value'
 # Update the admin user
-Install-User -UserName Administrator -Password "$adminPass"
+$adminCreds = New-Credential -UserName Administrator -Password "$adminPass"
+Install-User -Credential $adminCreds
