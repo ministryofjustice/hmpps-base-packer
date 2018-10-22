7ab2e809df0cfbe3f5de8b8ed9636a26e3df47ca
Brett Minnie <github@bdb-studios.co.uk>
2018-10-22 14:06:30 +0100

    Re-enable user data after build as windows disables it

---
+++ scripts/ReenableUserData.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = "Stop"
+$VerbosePreference="Continue"
+
+$EC2SettingsFile="C:\Program Files\Amazon\Ec2ConfigService\Settings\Config.xml"
+$xml = [xml](get-content $EC2SettingsFile)
+$xmlElement = $xml.get_DocumentElement()
+$xmlElementToModify = $xmlElement.Plugins
+
+foreach ($element in $xmlElementToModify.Plugin) {
+    if ($element.name -eq "Ec2HandleUserData") {
+        $element.State="Enabled"
+    }
+}
+$xml.Save($EC2SettingsFile)
