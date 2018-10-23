d46ad808e38342c0769f780604c8bb2ca9cc5602
Brett Minnie <github@bdb-studios.co.uk>
2018-10-23 12:58:29 +0100

    && not valid in powershell

--- scripts/WindowsMISNARTInstallRequirements.ps1
+++ scripts/WindowsMISNARTInstallRequirements.ps1
@@ -35,7 +35,7 @@ rmdir /s /q c:\salt\states
 
 git clone -b issue-1 https://github.com/ministryofjustice/hmpps-mis-saltstack-states.git c:\salt\states
 
-copy c:\salt\states\bootstrap\minion.conf C:\salt\conf\minion.d\minion.conf
+copy C:\salt\states\bootstrap\minion.conf C:\salt\conf\minion.d\minion.conf
 
 rmdir /s /q c:\salt\states
 
@@ -43,6 +43,6 @@ net stop salt-minion
 
 net start salt-minion
 
-salt-call pkg.refresh_db && salt-call state.highstate
+salt-call pkg.refresh_db; salt-call state.highstate
 
 echo "salt bootstrap end"
