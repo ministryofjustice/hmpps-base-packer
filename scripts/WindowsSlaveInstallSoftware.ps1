5eae24b7fb14824f7869bae49e6784e97891cd0b
Brett Minnie <github@bdb-studios.co.uk>
2018-10-24 15:22:16 +0100

    Broke up slave steps

--- scripts/WindowsSlaveInstallSoftware.ps1
+++ scripts/WindowsSlaveInstallSoftware.ps1
@@ -1,44 +1,9 @@
 $ErrorActionPreference = "Stop"
 $VerbosePreference="Continue"
 
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-
 #--- Git ---
-choco install git make -y
-
-#--- JDK 8 ---
-$javaDir = "C:\\java8"
-choco install jdk8 -params "installdir=$javaDir" -y
-[System.Environment]::SetEnvironmentVariable('JAVA_HOME', $javaDir, [System.EnvironmentVariableTarget]::Machine)
-# Set Java memory limits
-[System.Environment]::SetEnvironmentVariable('JAVA_OPTS',  '-Xms6144m -Xmx12288m', [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('_JAVA_OPTS',  '-Xms6144m -Xmx12288m', [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";%JAVA_HOME%\bin", [EnvironmentVariableTarget]::Machine)
-RefreshEnv
-$env:JAVA_HOME="$javaDir"
-
-#--- Download & Install Maven
-$url = "http://mirror.vorboss.net/apache/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.zip"
-$output = "C:\maven.zip"
-$maven_dir = "C:\maven"
-echo "Downloading $url to install to $maven_dir"
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $output)
-[System.IO.Compression.ZipFile]::ExtractToDirectory("$output", "C:\\")
-#Rename to something clean
-Rename-Item -Path "C:\\apache-maven-3.5.4" -NewName "$maven_dir"
-#Add maven to env and path
-[System.Environment]::SetEnvironmentVariable('M2_HOME', "$maven_dir", [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('MAVEN_HOME', "$maven_dir", [System.EnvironmentVariableTarget]::Machine)
-$env:MAVEN_HOME="$maven_dir"
-[System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";%MAVEN_HOME%\bin", [EnvironmentVariableTarget]::Machine)
-
+choco install git -y
+#--- Make
+choc install make -y
 #--- Install Non Sucking Service Manager
 choco install nssm -y
-
-#--- Download firefox
-$url = "https://ftp.mozilla.org/pub/firefox/releases/52.9.0esr/win64/en-US/Firefox%20Setup%2052.9.0esr.exe"
-$output = "$env:temp\setup.exe"
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $output)
-& "$env:temp\setup.exe" -ms /INI=c:\temp\firefox.ini
