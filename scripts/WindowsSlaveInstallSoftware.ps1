a47fc8a250953c91c1207e79b5c20df0e62017ee
Brett Minnie <github@bdb-studios.co.uk>
2018-10-24 13:11:39 +0100

    wurnderzzz

--- scripts/WindowsSlaveInstallSoftware.ps1
+++ scripts/WindowsSlaveInstallSoftware.ps1
@@ -12,9 +12,9 @@ if (!(Test-Path $ChocoInstallPath)) {
 choco install git make -y
 
 #--- JDK 8 ---
-$javaDir = "C:\java8"
+$javaDir = "C:\\java8"
 choco install jdk8 -params "installdir=$javaDir" -y
-[System.Environment]::SetEnvironmentVariable('JAVA_HOME', "$javaDir", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('JAVA_HOME', $javaDir, [System.EnvironmentVariableTarget]::Machine)
 # Set Java memory limits
 [System.Environment]::SetEnvironmentVariable('JAVA_OPTS',  '-Xms6144m -Xmx12288m', [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable('_JAVA_OPTS',  '-Xms6144m -Xmx12288m', [System.EnvironmentVariableTarget]::Machine)
