6984993d3b219a9678400ca03a73beea5aae72ce
Brett Minnie <github@bdb-studios.co.uk>
2018-10-25 14:28:34 +0100

    Inject env var

--- scripts/WindowsManageAdminPassword.ps1
+++ scripts/WindowsManageAdminPassword.ps1
@@ -3,7 +3,9 @@ $VerbosePreference="Continue"
 
 # Get our our password details from ssm
 $keyname = "/$env:TARGET_ENV/jenkins/windows/slave/admin/password"
-$admin_password = (aws --region eu-west-2 ssm get-parameters --with-decryption --names ${keyname})
+echo $keyname
+$admin_password = (aws --region eu-west-2 ssm get-parameters --with-decryption --names ${keyname} | jq -r '.Parameters[0].Value')
+echo $admin_password.length
 # Update the admin user
 $adminCreds = New-Credential -UserName Administrator -Password "$admin_password"
 Install-User -Credential $adminCreds
