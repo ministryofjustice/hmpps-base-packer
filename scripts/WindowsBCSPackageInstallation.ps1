6bc056ac3aaa1f7dae7da9b26b8af324162d85a0
Brett Minnie <github@bdb-studios.co.uk>
2018-10-26 15:26:04 +0100

    Added debug

--- scripts/WindowsBCSPackageInstallation.ps1
+++ scripts/WindowsBCSPackageInstallation.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 $VerbosePreference="Continue"
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-
+echo "s3://$env:ARTIFACT_BUCKET/mis/Data+Services+Related/win32_11gR2_client.zip"
 # Get our oracle packages from s3
 aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/Data+Services+Related/win32_11gR2_client.zip" c:\temp\win32_11gR2_client.zip
 aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/Data+Services+Related/win64_11gR2_client.zip" c:\temp\win64_11gR2_client.zip
