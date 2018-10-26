cec9d57fe405fdd8145d70368e8ca84f1f5e00b6
Brett Minnie <github@bdb-studios.co.uk>
2018-10-26 15:18:32 +0100

    AwsCLI vs commandlet

--- scripts/WindowsBCSPackageInstallation.ps1
+++ scripts/WindowsBCSPackageInstallation.ps1
@@ -3,8 +3,8 @@ $VerbosePreference="Continue"
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 # Get our oracle packages from s3
-Read-S3Object -BucketName $env:ARTIFACT_BUCKET -Key mis/Data+Services+Related/win32_11gR2_client.zip -File c:\temp\win32_11gR2_client.zip
-Read-S3Object -BucketName $env:ARTIFACT_BUCKET -Key mis/Data+Services+Related/win64_11gR2_client.zip -File c:\temp\win64_11gR2_client.zip
+aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/Data+Services+Related/win32_11gR2_client.zip" c:\temp\win32_11gR2_client.zip
+aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/Data+Services+Related/win64_11gR2_client.zip" c:\temp\win64_11gR2_client.zip
 
 [System.IO.Compression.ZipFile]::ExtractToDirectory("c:\\temp\\win32_11gR2_client.zip", "C:\\temp")
 [System.IO.Compression.ZipFile]::ExtractToDirectory("c:\\temp\\win64_11gR2_client.zip", "C:\\temp")
