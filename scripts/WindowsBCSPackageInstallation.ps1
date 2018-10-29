0120fe1a73a1d2e33c21e71bb290246ed3dc8c7a
Brett Minnie <github@bdb-studios.co.uk>
2018-10-29 10:25:41 +0000

    Stop after first extraction so we can interrogate the system

--- scripts/WindowsBCSPackageInstallation.ps1
+++ scripts/WindowsBCSPackageInstallation.ps1
@@ -7,6 +7,8 @@ aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/Data Services Related/win32_11gR2_clien
 aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/Data Services Related/win64_11gR2_client.zip" c:\temp\win64_11gR2_client.zip
 
 [System.IO.Compression.ZipFile]::ExtractToDirectory("c:\\temp\\win32_11gR2_client.zip", "C:\\temp")
-[System.IO.Compression.ZipFile]::ExtractToDirectory("c:\\temp\\win64_11gR2_client.zip", "C:\\temp")
+
+
+#[System.IO.Compression.ZipFile]::ExtractToDirectory("c:\\temp\\win64_11gR2_client.zip", "C:\\temp")
 
 
