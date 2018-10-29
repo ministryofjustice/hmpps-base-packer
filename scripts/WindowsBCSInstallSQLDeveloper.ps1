6b8d5e0bed16b65509a1266807042b1ef158b01a
Brett Minnie <github@bdb-studios.co.uk>
2018-10-29 17:43:23 +0000

    Renamed artifact

--- scripts/WindowsBCSInstallSQLDeveloper.ps1
+++ scripts/WindowsBCSInstallSQLDeveloper.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 $VerbosePreference="Continue"
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
-aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/sqldeveloper-18.3.0.277.2354-no-jre.zip" d:\sqldeveloper.zip
+aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/oracle-sqldeveloper.zip" d:\sqldeveloper.zip
 [System.IO.Compression.ZipFile]::ExtractToDirectory("D:\\sqldeveloper.zip", "D:\\")
 Remove-Item –path D:\sqldeveloper.zip –recurse
 [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";D:\sqldeveloper", [EnvironmentVariableTarget]::Machine)
