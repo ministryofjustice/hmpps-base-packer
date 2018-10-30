1a71df2f5d6a49366fca944290a09a27e4847070
Brett Minnie <github@bdb-studios.co.uk>
2018-10-30 13:29:35 +0000

    Correct path for binary

--- scripts/Windows/Base/InstallOpenSSH.ps1
+++ scripts/Windows/Base/InstallOpenSSH.ps1
@@ -3,7 +3,7 @@ $VerbosePreference="Continue"
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 # Get our oracle packages from s3
-aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/Data Services Related/OpenSSH-Win64.zip" c:\temp\OpenSSH-Win64.zip
+aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/OpenSSH-Win64.zip" c:\temp\OpenSSH-Win64.zip
 
 #Ensure our folder is created with the correct permissions
 $openssh_install_path = "C:\Program Files\OpenSSH"
---
+++ scripts/Windows/Base/YamlSupport.ps1
@@ -0,0 +1,5 @@
+$ErrorActionPreference = "Stop"
+$VerbosePreference="Continue"
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$psYaml="https://github.com/Phil-Factor/PSYaml/archive/master.zip"
