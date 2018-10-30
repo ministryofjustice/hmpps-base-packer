5c5a30a91e142375b4c66b5a4d7f5087daa5fdf8
Brett Minnie <github@bdb-studios.co.uk>
2018-10-30 10:01:00 +0000

    Ignore errors, list drive contents

--- scripts/WindowsBCSInstallSQLDeveloper.ps1
+++ scripts/WindowsBCSInstallSQLDeveloper.ps1
@@ -2,7 +2,11 @@ $ErrorActionPreference = "Stop"
 $VerbosePreference="Continue"
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
-aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/oracle-sqldeveloper.zip" d:\sqldeveloper.zip
+aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/oracle-sqldeveloper.zip" D:\sqldeveloper.zip
 [System.IO.Compression.ZipFile]::ExtractToDirectory("D:\\sqldeveloper.zip", "D:\\")
-Remove-Item –path D:\sqldeveloper.zip –recurse
+
+Get-PSDrive
+Get-ChildItem -Path D:\
+
+Remove-Item –path D:\sqldeveloper.zip –Recurse -ErrorAction Ignore
 [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";D:\sqldeveloper", [EnvironmentVariableTarget]::Machine)
