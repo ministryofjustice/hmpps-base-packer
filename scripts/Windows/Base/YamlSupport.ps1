bf8dda61380f6b6e93954f6ba163a237b3cf252d
Brett Minnie <github@bdb-studios.co.uk>
2018-10-30 15:37:16 +0000

    Install psyaml

--- scripts/Windows/Base/YamlSupport.ps1
+++ scripts/Windows/Base/YamlSupport.ps1
@@ -2,4 +2,7 @@ $ErrorActionPreference = "Stop"
 $VerbosePreference="Continue"
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
-$psYaml="https://github.com/Phil-Factor/PSYaml/archive/master.zip"
+# Get our oracle packages from s3
+aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/PSYaml-master.zip" C:\temp\PSYaml-master.zip
+[System.IO.Compression.ZipFile]::ExtractToDirectory("C:\\temp\\PSYaml-master.zip", "C:\\temp")
+mv C:\temp\PSYaml-master $env:USERPROFILE\Documents\WindowsPowerShell\Modules\
