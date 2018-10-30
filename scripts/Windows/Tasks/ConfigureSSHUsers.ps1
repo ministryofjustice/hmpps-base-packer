3e90f7792de9059200766ae911315aa898c8d614
Brett Minnie <github@bdb-studios.co.uk>
2018-10-30 17:01:28 +0000

    Load yaml content

--- scripts/Windows/Tasks/ConfigureSSHUsers.ps1
+++ scripts/Windows/Tasks/ConfigureSSHUsers.ps1
@@ -1,7 +1,19 @@
-if (-not (Test-Path env:TARGET_ENVIRONMENT)) { $env:TARGET_ENVIRONMENT = 'dev' }
+import-module psyaml
+
+if (-not (Test-Path env:TARGET_ENVIRONMENT)) {
+    $env:TARGET_ENVIRONMENT = 'dev'
+}
 
 $url = "https://github.com/ministryofjustice/hmpps-delius-ansible/blob/master/group_vars/$env:TARGET_ENVIRONMENT.yml"
 
 #download our yaml file
 Invoke-WebRequest -Uri "$url" -OutFile "$env:TEMP\users.yaml"
+
+[string[]]$fileContent = Get-Content "$env:TEMP\users.yaml"
+$content = ''
+foreach ($line in $fileContent) {
+    $content = $content + "`n" + $line
+}
+
+$yaml = ConvertFrom-YAML $content
 
