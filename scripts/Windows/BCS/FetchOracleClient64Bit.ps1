adaba59725bc6c13db4dffd2be65a585f16a2cd8
Brett Minnie <github@bdb-studios.co.uk>
2018-10-31 08:21:43 +0000

    Added latest version of client to build

--- scripts/Windows/BCS/FetchOracleClient64Bit.ps1
+++ scripts/Windows/BCS/FetchOracleClient64Bit.ps1
@@ -3,7 +3,7 @@ $VerbosePreference="Continue"
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 # Get our oracle packages from s3
-aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/Data Services Related/win64_11gR2_client.zip" c:\temp\win64_11gR2_client.zip
+aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/oracle_11204_win64_client.zip" c:\temp\win64_11gR2_client.zip
 
 #Unpack our installer
 [System.IO.Compression.ZipFile]::ExtractToDirectory("c:\\temp\\win64_11gR2_client.zip", "C:\\temp")
--- scripts/Windows/BCS/InstallOracleClient.bat
+++ scripts/Windows/BCS/InstallOracleClient.bat
@@ -1,1 +1,3 @@
+@ECHO ON
+
 c:\temp\ora64_installer\setup.exe -silent -nowelcome -nowait -noconfig  -responseFile c:\temp\ora64_client.rsp
