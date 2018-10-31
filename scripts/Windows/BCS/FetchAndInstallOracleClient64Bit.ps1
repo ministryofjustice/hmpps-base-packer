828c1428d28d141222ba1e3394cd6f4f4bb56cc9
Brett Minnie <github@bdb-studios.co.uk>
2018-10-31 11:54:12 +0000

    Updated StartProcess command

--- scripts/Windows/BCS/FetchAndInstallOracleClient64Bit.ps1
+++ scripts/Windows/BCS/FetchAndInstallOracleClient64Bit.ps1
@@ -10,12 +10,15 @@ aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/oracle_11204_win64_client.zip" c:\temp\
 Rename-Item -Path C:\temp\client -NewName c:\temp\ora64_installer
 
 $command = "c:\temp\ora64_installer\setup.exe"
-$arguments = "-silent -nowelcome -nowait -noconfig  -responseFile c:\temp\ora64_client.rsp -showProgress"
+$arguments = "-silent -nowelcome -nowait -noconfig -responseFile c:\temp\ora64_client.rsp -showProgress"
 
 #Run our process in the foreground, wait till spawned window terminates
-Start-Process $cmd "$arguments" -NoNewWindow -Wait
+Start-Process -FilePath $command -ArgumentList $arguments -NoNewWindow -Wait
 
-$logFiles = Get-ChildItem "$env:SYSTEM_DRIVE\Program Files\Oracle\Inventory\logs\silent*.log"
+#Windows is as windows does
+Start-Sleep -Seconds 30
+
+$logFiles = Get-ChildItem "$env:SYSTEM_DRIVE\Program Files\Oracle\Inventory\logs\silent*.log" -ErrorAction Ignore
 
 foreach ($logFile in $logfiles.Name) {
     type "$env:SYSTEM_DRIVE\Program Files\Oracle\Inventory\logs\$logFile"
