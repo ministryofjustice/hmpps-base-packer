65f78aaed4b7a450d52e4312c4f8d168d7a6e730
Brett Minnie <github@bdb-studios.co.uk>
2018-10-31 13:22:56 +0000

    Bloody oracle

--- scripts/Windows/BCS/FetchAndInstallOracleClient64Bit.ps1
+++ scripts/Windows/BCS/FetchAndInstallOracleClient64Bit.ps1
@@ -9,8 +9,13 @@ aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/oracle_11204_win64_client.zip" c:\temp\
 [System.IO.Compression.ZipFile]::ExtractToDirectory("c:\\temp\\win64_11gR2_client.zip", "C:\\temp")
 Rename-Item -Path C:\temp\client -NewName c:\temp\ora64_installer
 
+#Get our current IP address
+$my_ip = curl  http://169.254.169.254/latest/meta-data/local-ipv4
+$my_ip = $my_ip.Content
+
+#Run our installer
 $command = "c:\temp\ora64_installer\setup.exe"
-$arguments = "-silent -nowelcome -nowait -noconfig -responseFile c:\temp\ora64_client.rsp -showProgress"
+$arguments = "-silent -nowelcome -nowait -noconfig -responseFile c:\temp\ora64_client.rsp -showProgress oracle.install.client.schedulerAgentHostName=$my_ip"
 
 #Run our process in the foreground, wait till spawned window terminates
 Start-Process -FilePath $command -ArgumentList $arguments -NoNewWindow -Wait
