a2c5b61b7803b03f5e3aa2559a2804ce15a46822
Brett Minnie <github@bdb-studios.co.uk>
2018-11-01 15:41:25 +0000

    Fixed escape issue in string

--- scripts/Windows/JenkinsSlave/InstallJDK8.ps1
+++ scripts/Windows/JenkinsSlave/InstallJDK8.ps1
@@ -3,7 +3,7 @@ $VerbosePreference="Continue"
 
 #--- JDK 8 ---
 $javaDir = "C:\java8"
-choco install jdk8 -params "installdir=$javaDir" -y
+choco install jdk8 -params "installdir=C:\\Java8" -y
 [System.Environment]::SetEnvironmentVariable('JAVA_HOME', $javaDir, [System.EnvironmentVariableTarget]::Machine)
 # Set Java memory limits
 [System.Environment]::SetEnvironmentVariable('JAVA_OPTS',  '-Xms6144m -Xmx12288m', [System.EnvironmentVariableTarget]::Machine)
