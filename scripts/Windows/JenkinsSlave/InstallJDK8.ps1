24bb581c63c113e5e925fde7e8089d8c3fd4cd7e
Brett Minnie <github@bdb-studios.co.uk>
2018-11-01 15:01:38 +0000

    Updated env paths for java

--- scripts/Windows/JenkinsSlave/InstallJDK8.ps1
+++ scripts/Windows/JenkinsSlave/InstallJDK8.ps1
@@ -2,10 +2,10 @@ $ErrorActionPreference = "Stop"
 $VerbosePreference="Continue"
 
 #--- JDK 8 ---
-$javaDir = "C:\\java8"
+$javaDir = "C:\java8"
 choco install jdk8 -params "installdir=$javaDir" -y
 [System.Environment]::SetEnvironmentVariable('JAVA_HOME', $javaDir, [System.EnvironmentVariableTarget]::Machine)
 # Set Java memory limits
 [System.Environment]::SetEnvironmentVariable('JAVA_OPTS',  '-Xms6144m -Xmx12288m', [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable('_JAVA_OPTS',  '-Xms6144m -Xmx12288m', [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";%JAVA_HOME%\bin", [EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$javaDir\bin;$javaDir", [EnvironmentVariableTarget]::Machine)
