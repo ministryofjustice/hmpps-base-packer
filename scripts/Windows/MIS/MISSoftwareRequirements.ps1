7dc5e471810245d80c8ee54b658023eba16b98a6
Brett Minnie <github@bdb-studios.co.uk>
2018-11-01 12:50:57 +0000

    Install DOTNET3.5 using windows features

--- scripts/Windows/MIS/MISSoftwareRequirements.ps1
+++ scripts/Windows/MIS/MISSoftwareRequirements.ps1
@@ -5,4 +5,9 @@ $VerbosePreference="Continue"
 choco install vcredist2012 vcredist2015 vcredist-all -y
 
 #--- DotNet 4.5 ---
-choco install dotnet3.5 dotnet4.5 dotnet4.5.2 -y
+choco install dotnet4.5 dotnet4.5.2 -y
+
+#Get .net3.5
+aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/dotNetFx35setup.exe" c:\temp\dotNetFx35setup.exe
+#Install .net3.5
+Install-WindowsFeature Net-Framework-Core -source C:\temp
