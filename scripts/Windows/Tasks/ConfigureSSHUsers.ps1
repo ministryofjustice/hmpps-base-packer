8854ae976a6d863aa8fc4281546c5ea43de1ce29
Brett Minnie <github@bdb-studios.co.uk>
2018-11-01 09:29:13 +0000

    Updated create user script

--- scripts/Windows/Tasks/ConfigureSSHUsers.ps1
+++ scripts/Windows/Tasks/ConfigureSSHUsers.ps1
@@ -4,6 +4,7 @@ if (-not (Test-Path env:TARGET_ENVIRONMENT)) {
     $env:TARGET_ENVIRONMENT = 'dev'
 }
 
+
 $url = "https://github.com/ministryofjustice/hmpps-delius-ansible/blob/master/group_vars/$env:TARGET_ENVIRONMENT.yml"
 
 #download our yaml file
@@ -17,3 +18,37 @@ foreach ($line in $fileContent) {
 
 $yaml = ConvertFrom-YAML $content
 
+foreach($group in $yaml.groups_to_create) {
+    if(-not (Get-Group -Name ${group.name})) {
+        Install-Group -Name $group.name
+    }
+}
+
+foreach($user in $yaml.users) {
+    $keyname = "/$env:TARGET_ENVIRONMENT/${user['username']}/windows/rds/password"
+
+    if (-not (Get-User -Name ${user['username']})) {
+        #If we don't have a password we generate one
+        if(-not $password) {
+            $passwordLen = (Get-Random -Minimum 32 -Maximum 64)
+            $password = [System.Web.Security.Membership]::GeneratePassword($passwordLen, 10)
+            aws --region eu-west-2 ssm put-parameter --name $keyname --type SecureString --value $password
+        }
+
+    } else {
+        $password = aws --region eu-west-2 ssm get-parameter --with-decryption --name $keyname --query Parameters[0].Value
+    }
+
+    $creds = New-Credentials -User $user['username'] -Password $password
+    Install-User -Credentials $creds -FullName "$user['name']"
+    if ($user['groups'] contains 'wheel') {
+        Add-GroupMember -Name Administrators -Member "$user['username']"
+    }
+    foreach ($group in $user['groups]) {
+        if ($group -ne 'adm' || $group -ne 'wheel') {
+            Add-GroupMember -Name $group -Member "$user['username']"
+
+        }
+    }
+
+}
