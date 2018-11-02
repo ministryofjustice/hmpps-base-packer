a46a81a798dfe75e099ffd31032e28015713d07e
Brett Minnie <github@bdb-studios.co.uk>
2018-11-02 13:27:23 +0000

    Install VCRedist we hope

--- scripts/Windows/MIS/MISSoftwareRequirements.ps1
+++ scripts/Windows/MIS/MISSoftwareRequirements.ps1
@@ -13,3 +13,12 @@ choco install dotnet4.5 dotnet4.5.2 -y
 aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/dotNetFx35setup.exe" c:\temp\dotNetFx35setup.exe
 #Install .net3.5
 Install-WindowsFeature Net-Framework-Core -source C:\temp
+
+#Get M$ VC++ redist
+
+aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/vc_redist.x64.exe" c:\temp\vc_redist.x64.exe
+. c:\temp\vc_redist.x64.exe /install /passive /norestart
+if ($? -eq $true) {
+    exit 0
+}
+exit 1
