c36b3c6f5ccce002fed8ae2f2bd3b1a16cd50c03
Brett Minnie <github@bdb-studios.co.uk>
2018-11-06 12:21:16 +0000

    Get our epel file

---
+++ scripts/ansible_galaxy_base.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo cp /usr/share/zoneinfo/Europe/London /etc/localtime
+
+sudo yum install -y yum-utils \
+     device-mapper-persistent-data \
+     lvm2 \
+     gcc \
+     gcc-c++ \
+     openssl-devel \
+     make \
+     git \
+     java-1.8.0-openjdk \
+     python-pip \
+     jq
+
+sudo amazon-linux-extras install -y docker
+
+cd /tmp
+wget https://github.com/AGWA/git-crypt/archive/0.6.0.tar.gz
+tar -xf 0.6.0.tar.gz
+cd git-crypt-0.6.0
+sudo make
+sudo make install PREFIX=/usr/local
+cd /tmp
+rm -rf *0.6.0*
+
+echo "vm.max_map_count=262144" |  sudo tee -a /etc/sysctl.conf
+sudo systemctl start docker
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo pip install virtualenv awscli boto ansible==2.6 botocore boto3
+sudo curl -fsSL https://goss.rocks/install | sudo sh
+
+sudo usermod -aG docker ec2-user
+
+sudo chmod +x /home/ec2-user/configure_github
+
+#install maven
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+mvn --version
+
+
--- scripts/centos_slave.sh
+++ scripts/centos_slave.sh
@@ -6,3 +6,7 @@ sudo yum install -y \
     python-pip
 
 sudo pip install ansible==2.6 virtualenv awscli boto botocore boto3
+
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+
