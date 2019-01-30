9e4b27beb3c7d9feb2d0f7f7bc8d6f8564d569a5
Brett Minnie <github@bdb-studios.co.uk>
2019-01-30 14:32:17 +0000

    Find our galaxyfile

--- generate_metadata.py
+++ generate_metadata.py
@@ -4,12 +4,34 @@ import json
 import sys
 
 
+def find_json_value(json_obj, key):
+
+    def _(dictobj, lookup):
+        if lookup in dictobj.keys():
+            return dictobj[lookup]
+        else:
+            for sub_dictobj in [d for d in dictobj.values() if type(d) == dict]:
+                result = _(sub_dictobj, lookup)
+                if result:
+                    return result
+
+            # if objects in dictobj.values() are lists, go through them
+            for listobject in [l for l in dictobj.values() if type(l) == list]:
+                for sub_dictobj in [d for d in listobject if type(d) == dict]:
+                    result = _(sub_dictobj, lookup)
+                    if result:
+                        return result
+            return None
+
+    return _(json_obj, key)
+
+
 def extract_galaxy_libs(filename):
     with open(filename) as json_data:
         data = json.load(json_data)
         json_data.close()
 
-    print(data)
+    print(find_json_value(data, 'galaxy_file'))
 
 
 if __name__ == "__main__":
