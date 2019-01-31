41ff1fb55c79a37a6c62186d64903f1c9f335587
Stephen James <39086019+smjmoj@users.noreply.github.com>
2019-01-31 10:12:39 +0000

    Removed ntp install. Not required as Centos has chrony already enabled (#73)
    https://www.thegeekdiary.com/centos-rhel-7-chrony-vs-ntp-differences-between-ntpd-and-chronyd/

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,11 +3,3 @@
 sudo yum install -y deltarpm
 
 sudo yum -y update
-
-
-# Install NTP Services so our system time is always in sync
-sudo yum install -y ntp ntpdate ntp-doc
-sudo service ntpd stop # Just to make sure
-sudo chkconfig ntpd on
-sudo ntpdate pool.ntp.org
-sudo service ntpd start
