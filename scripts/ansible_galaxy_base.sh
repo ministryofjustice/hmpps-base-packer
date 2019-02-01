ff937f659466407c977e99a7ad48d7bc751052af
smjmoj <smjmoj@gmail.com>
2019-02-01 16:11:16 +0000

    Fixed no package "python-pip" with "python2-pip"

--- scripts/ansible_galaxy_base.sh
+++ scripts/ansible_galaxy_base.sh
@@ -3,11 +3,8 @@
 # Installing locally so in future we can run ansible to boostrap our instances
 sudo yum install -y \
     git \
-    python-pip \
+    python2-pip \
     wget \
     yum-utils
 
 sudo pip install ansible==2.6 virtualenv awscli boto botocore boto3
-
-
-
