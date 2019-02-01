d2b0b9dda16a6594d90820ccd6e29d7d7e8750da
smjmoj <smjmoj@gmail.com>
2019-02-01 16:22:07 +0000

    Let's try another pip install

--- scripts/ansible_galaxy_base.sh
+++ scripts/ansible_galaxy_base.sh
@@ -3,8 +3,9 @@
 # Installing locally so in future we can run ansible to boostrap our instances
 sudo yum install -y \
     git \
-    python2-pip \
     wget \
     yum-utils
+
+sudo easy_install pip
 
 sudo pip install ansible==2.6 virtualenv awscli boto botocore boto3
