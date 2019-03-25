8d1f8b06a9e2745849c32da962d0f8a71d23cb82
Brett Minnie <github@bdb-studios.co.uk>
2019-03-25 13:01:11 +0000

    Centos base with reboot (#91)
    * Centos base with reboot
    
    * Enable ipforwarding
    
    * Extra space
    
    * Suggested yum cache clean
    
    * Reboot workaround
    
    * Add the centos user to the docker group
    
    * Added reboot output
    
    * Revert to main jenkinsfile

---
+++ scripts/update_centos7.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Enable ipv4 and ipv6 forwarding for docker and other tooling
+sudo /sbin/sysctl -w net.ipv4.conf.all.forwarding=1
+sudo /sbin/sysctl -w net.ipv6.conf.all.forwarding=1
+sudo /sbin/sysctl -p
+
+# Create our meta-data directory
+sudo mkdir -p /opt/meta-data/
+sudo chmod -R 777 /opt/meta-data
+
+sudo yum install -y deltarpm
+
+sudo yum clean all
+sudo rm -rf /var/cache/yum
+sudo yum update -y --exclude=polkit*
+echo "Rebooting our system"
+sudo systemctl stop sshd.service
+sudo nohup shutdown -r now < /dev/null > /dev/null 2>&1 &
+exit 0
