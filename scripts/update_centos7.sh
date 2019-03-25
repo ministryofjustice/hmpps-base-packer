63d9196085ff6a0ca75385c087d017c115176065
Brett Minnie <github@bdb-studios.co.uk>
2019-03-25 12:08:32 +0000

    Enable ipforwarding

--- scripts/update_centos7.sh
+++ scripts/update_centos7.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Enable ipv4 and ipv6 forwarding for docker and other tooling
+sudo /sbin/sysctl -w net.ipv4.conf.all.forwarding=1
+sudo /sbin/sysctl -w net.ipv6.conf.all.forwarding=1
+sudo /sbin/sysctl -p
+
+# Create our meta-data directory
 sudo mkdir -p /opt/meta-data/
 sudo chmod -R 777 /opt/meta-data
 
