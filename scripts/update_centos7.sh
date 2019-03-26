76e71bbc7a09ee45465d3ced29a2e5350f745416
Brett Minnie <github@bdb-studios.co.uk>
2019-03-26 10:38:01 +0000

    IP forwarding set in ansbile docker role

--- scripts/update_centos7.sh
+++ scripts/update_centos7.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-echo "net.ipv4.conf.all.forwarding=1" | sudo tee -a /etc/sysctl.conf
-sudo sed -i 's/net.ipv6.conf.all.forwarding=0/net.ipv6.conf.all.forwarding=1/' /etc/sysctl.conf
-sudo sed -i 's/net.ipv4.ip_forward=0/net.ipv4.ip_forward=1/' /etc/sysctl.conf
-sudo /sbin/sysctl -p
-
 # Create our meta-data directory
 sudo mkdir -p /opt/meta-data/
 sudo chmod -R 777 /opt/meta-data
