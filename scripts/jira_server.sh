d3d4dfb2057d6731bd8b73a858673038faa671aa
John Barber <jsbarber@gmail.com>
2019-09-11 12:18:12 +0100

    sep jenkins file for testing

--- scripts/jira_server.sh
+++ scripts/jira_server.sh
@@ -2,7 +2,9 @@
 set -e
 
 sudo cp /usr/share/zoneinfo/Europe/London /etc/localtime
-sudo yum install -y yum-utils \
-sudo yum install -y wget git python-pip
+sudo yum install -y yum-utils wget git python-pip
 sudo pip install -U pip
 sudo pip install virtualenv ansible==2.6 boto3 botocore
+
+sudo chmod +x /home/ec2-user/configure_github
+
