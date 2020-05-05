5d209f1dbe6ec43eb7c661b01a09e41f54839c61
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-05-05 14:50:31 +0100

    Updated DotNet35 Install

--- scripts/Windows/MIS/MISSoftwareRequirements.ps1
+++ scripts/Windows/MIS/MISSoftwareRequirements.ps1
@@ -11,8 +11,9 @@ choco install dotnet4.5 dotnet4.5.2 -y
 
 #Get .net3.5
 aws s3 cp "s3://$env:ARTIFACT_BUCKET/mis/dotNetFx35setup.exe" c:\temp\dotNetFx35setup.exe
+
 #Install .net3.5
-Install-WindowsFeature Net-Framework-Core -source C:\temp
+#Install-WindowsFeature Net-Framework-Core -source C:\temp
 
 #Get M$ VC++ redist
 
