e1a0bb89ab1560e0564c518ebdee2ec231d574c2
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-05-05 13:33:11 +0100

    Updated code to disable IE Enhanced Security

--- scripts/Windows/Base/DisableUAC.ps1
+++ scripts/Windows/Base/DisableUAC.ps1
@@ -6,3 +6,5 @@ Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorUser" -Value "0"
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "1"
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Value "0"
+Write-Host "User Access Control (UAC) has been disabled." -ForegroundColor Green  
+
