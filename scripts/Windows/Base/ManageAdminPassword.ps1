07b2b8fec354434a4fd3769b3b05f56a0f68c530
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-11-23 10:12:52 +0000

    updated last remaining slave remnants from builds

--- scripts/Windows/Base/ManageAdminPassword.ps1
+++ scripts/Windows/Base/ManageAdminPassword.ps1
@@ -5,7 +5,7 @@ Import-Module 'Carbon'
 
 
 # Get our our password details from ssm
-#$keyname = "/$env:TARGET_ENV/jenkins/windows/slave/admin/password"
+#$keyname = "/$env:TARGET_ENV/jenkins/windows/agent/admin/password"
 #echo $env:TARGET_ENV
 #$admin_password = (aws --region eu-west-2 ssm get-parameters --with-decryption --names ${keyname} --query Parameters[0].Value )
 #echo ($admin_password -matches $env:WIN_ADMIN_PASS)
