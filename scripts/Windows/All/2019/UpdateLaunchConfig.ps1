8c09ec70ede0e5d36ca354e576694f68e5fdf2a5
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-03-12 13:03:25 +0000

    Updated Win2019 Packer builds for EC2-Launch

---
+++ scripts/Windows/All/2019/UpdateLaunchConfig.ps1
@@ -0,0 +1,12 @@
+$launchConfig = Get-Content -Path C:\ProgramData\Amazon\EC2-Windows\Launch\Config\LaunchConfig.json | ConvertFrom-Json
+
+# Update Administrator Password to the one we pass into the Packer Build from SSM ParameterStore
+$launchConfig.adminPasswordType = 'Specify'
+$launchConfig.adminPassword = "$env:WIN_ADMIN_PASS"
+
+# set the computername property
+$launchConfig.setComputerName = 'true'
+
+$launchConfig
+
+Set-Content -Value ($launchConfig | ConvertTo-Json) -Path C:\ProgramData\Amazon\EC2-Windows\Launch\Config\LaunchConfig.json
