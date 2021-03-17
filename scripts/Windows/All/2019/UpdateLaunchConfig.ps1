41289406c6bb15421a00d2e0652d5ca6929d4cd9
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-03-17 12:26:44 +0000

    Added step to set admin password in LaunchConfig.json

--- scripts/Windows/All/2019/UpdateLaunchConfig.ps1
+++ scripts/Windows/All/2019/UpdateLaunchConfig.ps1
@@ -2,11 +2,9 @@ $launchConfig = Get-Content -Path C:\ProgramData\Amazon\EC2-Windows\Launch\Confi
 
 # Update Administrator Password to the one we pass into the Packer Build from SSM ParameterStore
 $launchConfig.adminPasswordType = 'Specify'
-$launchConfig.adminPassword = "$env:WIN_ADMIN_PASS"
-
+$launchConfig.adminPassword     = "$env:WIN_ADMIN_PASS"
 # set the computername property
-$launchConfig.setComputerName = 'true'
-
+$launchConfig.setComputerName = 'True'
 $launchConfig
 
 Set-Content -Value ($launchConfig | ConvertTo-Json) -Path C:\ProgramData\Amazon\EC2-Windows\Launch\Config\LaunchConfig.json
