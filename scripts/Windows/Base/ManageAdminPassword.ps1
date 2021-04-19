8428df1a226250e4d53c35b9b6bcf6c23af10420
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-04-19 12:08:54 +0100

    removed debug code from createuser script

--- scripts/Windows/Base/ManageAdminPassword.ps1
+++ scripts/Windows/Base/ManageAdminPassword.ps1
@@ -3,14 +3,6 @@ $VerbosePreference="Continue"
 
 Import-Module 'Carbon'
 
-
-# Get our our password details from ssm
-#$keyname = "/$env:TARGET_ENV/jenkins/windows/agent/admin/password"
-#echo $env:TARGET_ENV
-#$admin_password = (aws --region eu-west-2 ssm get-parameters --with-decryption --names ${keyname} --query Parameters[0].Value )
-#echo ($admin_password -matches $env:WIN_ADMIN_PASS)
 # Update the admin user
 $adminCreds = New-Credential -UserName Administrator -Password "$env:WIN_ADMIN_PASS"
 Install-User -Credential $adminCreds
-
-
