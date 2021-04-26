0df420a86b764d771e4113b811651af97ab6c08f
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-04-26 10:57:22 +0100

    added sleep statement to allow debugging packer instance via ssm

--- scripts/Windows/CPOracle/CoreRequirements.ps1
+++ scripts/Windows/CPOracle/CoreRequirements.ps1
@@ -8,6 +8,11 @@ Invoke-WebRequest https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest
 Start-Process -FilePath $env:USERPROFILE\Desktop\SSMAgent_latest.exe -ArgumentList "/S"
 
 Write-Output "----------------------------------------------"
+Write-Output "Sleep instance for 1 hour to debug"
+Write-Output "----------------------------------------------"
+Start-Sleep -Seconds 3600
+
+Write-Output "----------------------------------------------"
 Write-Output "Install IIS Webserver, .NET Framework 4.5"
 Write-Output "----------------------------------------------"
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
@@ -17,26 +22,26 @@ Get-WindowsFeature | where { $_.InstallState -eq "Installed" } | Format-Table
 Write-Output "----------------------------------------------"
 Write-Output "Install IIS Rewrite Module"
 Write-Output "----------------------------------------------"
+Write-Output "Copy-S3Object.."
 Copy-S3Object -BucketName tf-eu-west-2-hmpps-eng-dev-artefacts-cporacle-s3bucket -Key website_dependencies/rewrite_amd64_en-US.msi -DestinationKey C:\Setup\rewrite_amd64_en-US.msi -Region eu-west-2
-# Install the downloaded msi
+
+Write-Output "Install the downloaded msi.."
 Start-Process -FilePath C:\Setup\rewrite_amd64_en-US.msi -ArgumentList "/quiet"
 
 Write-Output "----------------------------------------------"
 Write-Output "Newtonsoft.Json.dll"
 Write-Output "----------------------------------------------"
+Write-Output "Copy-S3Object.."
 Copy-S3Object -BucketName tf-eu-west-2-hmpps-eng-dev-artefacts-cporacle-s3bucket -Key website_dependencies/Newtonsoft.Json.dll -LocalFile c:\setup\Newtonsoft.Json.dll -Region eu-west-2
 
 Write-Output "Unblocking file c:\setup\Newtonsoft.Json.dll v"
 Unblock-File -Path c:\setup\Newtonsoft.Json.dll
 
-#  add dll to .NET GAC
-#Note that you should be running PowerShell as an Administrator
+Write-Output "Add dll to .NET GAC.."
 [System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")            
 $publish = New-Object System.EnterpriseServices.Internal.Publish            
 $publish.GacInstall("C:\Setup\Newtonsoft.Json.dll") 
 
-# Create target location for website logs
+Write-Output "Create target location 'C:\CPOracle\logs' for website logs.."
 New-Item -ItemType Directory -Path C:\CPOracle
 New-Item -ItemType Directory -Path C:\CPOracle\logs
-
- 
