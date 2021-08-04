5027339d42fa50cbd39ff73fc548b73038420f9e
swestb <84386015+swestb@users.noreply.github.com>
2021-08-04 09:55:31 +0100

    Rework of build scripts
    Now downloading source to AMI first.

---
+++ scripts/Windows/CPOracle/DownloadSource.ps1
@@ -0,0 +1,31 @@
+
+$VerbosePreference="Continue"
+Set-ExecutionPolicy Bypass -Force
+
+if( $false -eq (Test-Path -Path "C:\setup")) {
+New-Item -Path "c:\" -Name "setup" -ItemType "directory"
+}
+
+Write-Output "#----------------------------------------------------------------------"
+Write-Output "# Download CPOracle API Binaries from S3 Bucket "
+Write-Output "#----------------------------------------------------------------------"
+
+##### UPDATE $KarmaAPIVersion with the API release to deploy #####
+$KarmaAPIVersion   = "LondonCrc.Karma.API.2021.06.03.10.43"
+Write-Output ("Will download Karma API version: ${KarmaAPIVersion}")
+$KarmaAPIFileName = "${KarmaAPIVersion}.zip"
+
+Write-Output "#----------------------------------------------------------------------"
+Write-Output "Copying Karma Zip file from s3://$Bucket/$KarmaFileName"
+Write-Output "#----------------------------------------------------------------------"
+Read-S3Object -BucketName tf-eu-west-2-hmpps-eng-dev-artefacts-cporacle-s3bucket -Key /$KarmaAPIFileName -File c:\setup\$KarmaAPIFileName -Region eu-west-2
+
+##### UPDATE $KarmaWEBVersion with the Website release to deploy #####
+$KarmaWEBVersion   = "LondonCrc.Karma.Website.2021.06.04.14.40"
+Write-Output ("Will download Karma API version: ${KarmaWEBVersion}")
+$KarmaWEBFileName = "${KarmaWEBVersion}.zip"
+
+Write-Output "#----------------------------------------------------------------------"
+Write-Output "Copying Karma Zip file from s3://$Bucket/$KarmaFileName"
+Write-Output "#----------------------------------------------------------------------"
+Read-S3Object -BucketName tf-eu-west-2-hmpps-eng-dev-artefacts-cporacle-s3bucket -Key /$KarmaWEBFileName -File c:\setup\$KarmaWEBFileName -Region eu-west-2
