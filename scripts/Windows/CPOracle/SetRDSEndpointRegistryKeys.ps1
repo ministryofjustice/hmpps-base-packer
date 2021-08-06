1ac02dfef413af86d27d1052aa73995df4dc27b0
swestb <84386015+swestb@users.noreply.github.com>
2021-08-06 14:31:45 +0100

    Fix wrong character -

--- scripts/Windows/CPOracle/SetRDSEndpointRegistryKeys.ps1
+++ scripts/Windows/CPOracle/SetRDSEndpointRegistryKeys.ps1
@@ -19,8 +19,8 @@ Write-Output("get ssm param $cporacle_app_password_SSMPath")
 $cporacle_app_password = Get-SSMParameter -Name $cporacle_app_password_SSMPath -WithDecryption $true
 Write-Output("cporacle_app_password: " + $cporacle_app_password.Value)
 
-New-Item -Path HKLM:\Software\HMMPS -Name rdsendpoint –Force -Value “$RDSEndpoint”
-New-Item -Path HKLM:\Software\HMMPS -Name cporacleappuser –Force -Value “$cporacle_app_username.Value”
-New-Item -Path HKLM:\Software\HMMPS -Name cporacleapppw –Force -Value “$cporacle_app_password.Value”
+New-Item -Path HKLM:\Software\HMMPS -Name rdsendpoint -Force -Value “$RDSEndpoint”
+New-Item -Path HKLM:\Software\HMMPS -Name cporacleappuser -Force -Value “$cporacle_app_username.Value”
+New-Item -Path HKLM:\Software\HMMPS -Name cporacleapppw -Force -Value “$cporacle_app_password.Value”
 
 Write-Output('<---- End of of SetRDSEndpointRegistryKeys.ps1')
